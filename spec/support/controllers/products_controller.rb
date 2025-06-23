c9609debb82a787ac3644383b7a3e513a52e4522
Everson Silva <eversonsilva9799@gmail.com>
2025-06-23 20:07:54 -0300

    check if have authorization erro before process actions

--- spec/support/controllers/products_controller.rb
+++ spec/support/controllers/products_controller.rb
@@ -6,6 +6,17 @@ class ProductsController
   attr_accessor :params, :request, :current_user
   attr_reader :action_name
 
+  class AuthorizationError < StandardError
+    attr_reader :json, :status
+
+    def initialize(json: {}, status: nil)
+      @json = json
+      @status = status
+
+      super
+    end
+  end
+
   def initialize(current_user: nil, request: {}, params: {})
     @current_user = current_user
     @request = request
@@ -21,6 +32,8 @@ class ProductsController
       body: pagination_response.as_json,
       status: restme_scope_status
     }
+  rescue AuthorizationError => e
+    authorization_erro(e)
   end
 
   def show
@@ -31,6 +44,21 @@ class ProductsController
     {
       body: model_scope_object.as_json,
       status: restme_scope_status
+    }
+  rescue AuthorizationError => e
+    authorization_erro(e)
+  end
+
+  def render(json: {}, status: nil)
+    return unless status == :forbidden
+
+    raise AuthorizationError.new(json: json, status: status)
+  end
+
+  def authorization_erro(error)
+    {
+      body: error.json.as_json,
+      status: error.status
     }
   end
 end
