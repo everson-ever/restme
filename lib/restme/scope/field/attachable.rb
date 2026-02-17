e871bcc2cd99e45f470946961f714a8550b489e8
Everson Silva <eversonsilva9799@gmail.com>
2026-02-17 19:03:14 -0300

    remove class_eval from define_attachment_methods

--- lib/restme/scope/field/attachable.rb
+++ lib/restme/scope/field/attachable.rb
@@ -25,10 +25,13 @@ module Restme
 
         def define_attachment_methods
           attachment_fields_select.each do |attachment_field_name|
-            klass.class_eval do
-              define_method(:"#{attachment_field_name}_url") do
-                send(attachment_field_name).url
-              end
+            method_name = "#{attachment_field_name}_url"
+
+            next if klass.method_defined?(method_name)
+
+            klass.define_method(method_name) do
+              attachment = public_send(attachment_field_name)
+              attachment&.url
             end
           end
         end
